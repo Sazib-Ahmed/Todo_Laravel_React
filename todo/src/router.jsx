d8940cf9fa13cc1434fe8da3ecfd554f6e5a37fb
Sazib-Ahmed <mail.sazibahmed@gmail.com>
2023-09-26 01:12:05 +0600

    added react router

--- todo/src/router.jsx
+++ todo/src/router.jsx
@@ -1,6 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter ([
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/signup",
+    element: <signup/>
+  },
+  {
+    path: "/user",
+    element: <User/>
+  },
+  {
+    path: "/*",
+    element: <NotFound/>
+  },
+
 
 
 ])
